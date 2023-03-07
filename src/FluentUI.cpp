b24d54cd691c7759c11a35eabb3c426e6e629614
朱子楚\zhuzi <zhuzichu520@outlook.com>
2023-03-07 23:58:32 +0800

    update

--- src/FluentUI.cpp
+++ src/FluentUI.cpp
@@ -1,11 +1,10 @@
 ﻿#include "FluentUI.h"
-
 #include "Fluent.h"
-
 
 void FluentUI::registerTypes(const char *uri){
     Fluent::getInstance()->registerTypes(uri);
 }
+
 void FluentUI::initializeEngine(QQmlEngine *engine, const char *uri){
     Fluent::getInstance()->initializeEngine(engine,uri);
 }
