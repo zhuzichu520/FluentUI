fcb8da2c50b887eb94eb44c2e9a523b5d46a9332
朱子楚\zhuzi <zhuzichu520@outlook.com>
2023-04-04 02:37:20 +0800

    update

--- example/example.pro
+++ example/example.pro
@@ -25,18 +25,10 @@ CONFIG(debug,debug|release) {
 win32 {
 contains(QT_ARCH, i386) {
     COPYDLL = $$absolute_path($${_PRO_FILE_PWD_}/../third/Win_x86/*.dll) $$DESTDIR
-    contains(QMAKE_CC, cl) {
-        QMAKE_PRE_LINK += $$QMAKE_COPY $$replace(COPYDLL, /, \\)
-    } else {
-        QMAKE_PRE_LINK += $$QMAKE_COPY $$COPYDLL
-    }
+    QMAKE_PRE_LINK += $$QMAKE_COPY $$replace(COPYDLL, /, \\)
 } else {
     COPYDLL = $$absolute_path($${_PRO_FILE_PWD_}/../third/Win_x64/*.dll) $$DESTDIR
-    contains(QMAKE_CC, cl) {
-        QMAKE_PRE_LINK += $$QMAKE_COPY $$replace(COPYDLL, /, \\)
-    } else {
-        QMAKE_PRE_LINK += $$QMAKE_COPY $$COPYDLL
-    }
+    QMAKE_PRE_LINK += $$QMAKE_COPY $$replace(COPYDLL, /, \\)
 }
 }
 
--- src/win_install.bat
+++ src/win_install.bat
@@ -18,25 +18,12 @@ copy /y  %PWD_PATH%\FluentUI.h  %BUILDER_BIN_PATH% & copy /y  %PRESET_PATH%\*  %
 
 if %ANDROID% == YES copy /y %LIBFILE_PATH% %BUILDER_BIN_PATH%
 
-SET QMLPLUGINDUMP1_PATH=%QT_QML_FLUENT_PATH%\..\..\bin\qmlplugindump.exe
-SET QMLPLUGINDUMP2_PATH=%QT_QML_FLUENT_PATH%\..\..\bin\qmlplugindump-qt6.exe
-
 if %1 == SHARED (
     echo running install to qtqml folder
 	del /s /q %PRESET_PATH%\plugins.qmltypes
-	if exist %QMLPLUGINDUMP1_PATH% (
-		%QMLPLUGINDUMP1_PATH% -nonrelocatable FluentUI 1.0 > %PRESET_PATH%\plugins.qmltypes
-		rmdir /s /q %QT_QML_FLUENT_PATH% & md %QT_QML_FLUENT_PATH%
-		copy /y %BUILDER_BIN_PATH% %QT_QML_FLUENT_PATH%
-		xcopy %PRESET_PATH% %QT_QML_FLUENT_PATH% /s/e/i/y
-		cd %QT_QML_FLUENT_PATH%
-	) else (
-		SET QT_QML_FLUENT_PATH2 = %QT_QML_FLUENT_PATH%\..\..\share\qt6\qml\Fluent
-		%QMLPLUGINDUMP2_PATH% -nonrelocatable FluentUI 1.0 > %PRESET_PATH%\plugins.qmltypes
-		rmdir /s /q %QT_QML_FLUENT_PATH2% & md %QT_QML_FLUENT_PATH2%
-		copy /y %BUILDER_BIN_PATH% %QT_QML_FLUENT_PATH2%
-		xcopy %PRESET_PATH% %QT_QML_FLUENT_PATH2% /s/e/i/y
-		cd %QT_QML_FLUENT_PATH2%
-	)
-
+	%QT_QML_FLUENT_PATH%\..\..\bin\qmlplugindump.exe -nonrelocatable FluentUI 1.0 > %PRESET_PATH%\plugins.qmltypes
+    rmdir /s /q %QT_QML_FLUENT_PATH% & md %QT_QML_FLUENT_PATH%
+    copy /y %BUILDER_BIN_PATH% %QT_QML_FLUENT_PATH%
+	xcopy %PRESET_PATH% %QT_QML_FLUENT_PATH% /s/e/i/y
+	cd %QT_QML_FLUENT_PATH%
 )
