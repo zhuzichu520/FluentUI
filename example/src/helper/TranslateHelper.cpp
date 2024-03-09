eff9e385f53788301333c9b318af7445f4d06290
朱子楚\zhuzi <zhuzichu520@outlook.com>
2024-03-09 19:46:02 +0800

    update

--- example/src/helper/TranslateHelper.cpp
+++ example/src/helper/TranslateHelper.cpp
@@ -19,9 +19,6 @@ void TranslateHelper::init(QQmlEngine* engine){
     _translator = new QTranslator(this);
     qApp->installTranslator(_translator);
     QString translatorPath = QGuiApplication::applicationDirPath()+"/i18n";
-#ifdef Q_OS_MACX
-    translatorPath.append("/../Resources/");
-#endif
     _translator->load(QString::fromStdString("%1/example_%2.qm").arg(translatorPath,_current));
     _engine->retranslate();
 }
